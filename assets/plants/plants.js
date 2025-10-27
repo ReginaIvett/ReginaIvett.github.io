695ebf02980a3ca812a6b36c999c9f84e92d871e
ReginaIvett <reginaivettbracamontelopez@gmail.com>
2025-10-27 13:11:51 -0600

    Create plants.js with plant information database
    Added a database of plants with care instructions and images.

---
+++ assets/plants/plants.js
@@ -0,0 +1,23 @@
+// plants.js
+const plantsDB = {
+  "Monstera": { img:"assets/plants/monstera.jpg", info:"Water weekly. Indirect light. Repot every 2 years."},
+  "Aloe Vera": { img:"assets/plants/aloe_vera.jpg", info:"Water every 2-3 weeks. Full sun. Avoid overwatering."},
+  "Lavender": { img:"assets/plants/lavender.jpg", info:"Water weekly. Full sun. Prune after flowering."},
+  "Fiddle Leaf Fig": { img:"assets/plants/fiddle_leaf_fig.jpg", info:"Water weekly. Bright indirect light. Rotate monthly."},
+  "Sunflower": { img:"assets/plants/sunflower.jpg", info:"Water regularly. Full sun. Support tall stems."},
+  "Cactus": { img:"assets/plants/cactus.jpg", info:"Water sparingly. Full sun. Well-drained soil."},
+  "Rose": { img:"assets/plants/rose.jpg", info:"Water daily in summer. Full sun. Prune regularly."},
+  "Orchid": { img:"assets/plants/orchid.jpg", info:"Water weekly. Indirect light. Keep humidity high."},
+  "Bamboo": { img:"assets/plants/bamboo.jpg", info:"Water daily. Bright indirect light. Fertilize monthly."},
+  "Tulip": { img:"assets/plants/tulip.jpg", info:"Water moderately. Full sun. Plant bulbs in fall."},
+  "Mint": { img:"assets/plants/mint.jpg", info:"Water frequently. Partial sun. Harvest regularly."},
+  "Basil": { img:"assets/plants/basil.jpg", info:"Water often. Full sun. Pinch leaves to grow bushy."},
+  "Spider Plant": { img:"assets/plants/spider_plant.jpg", info:"Water weekly. Indirect light. Propagate by offsets."},
+  "Peace Lily": { img:"assets/plants/peace_lily.jpg", info:"Water weekly. Low light. Remove dead flowers."},
+  "Pothos": { img:"assets/plants/pothos.jpg", info:"Water weekly. Low to medium light. Easy to grow."},
+  "Geranium": { img:"assets/plants/geranium.jpg", info:"Water moderately. Full sun. Deadhead flowers."},
+  "Marigold": { img:"assets/plants/marigold.jpg", info:"Water moderately. Full sun. Protect from frost."},
+  "Daffodil": { img:"assets/plants/daffodil.jpg", info:"Water moderately. Full sun. Plant bulbs in fall."},
+  "Snake Plant": { img:"assets/plants/snake_plant.jpg", info:"Water sparingly. Low light. Very hardy."},
+  "Hibiscus": { img:"assets/plants/hibiscus.jpg", info:"Water regularly. Full sun. Fertilize monthly."}
+};
